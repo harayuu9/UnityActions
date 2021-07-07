d099f1b2308d1debfe0b0e4dc8115b96e375a2f6
harayuu9 <roudo2233@gmail.com>
2021-07-07 18:02:52 +0900

    no message

---
+++ test.bat
@@ -0,0 +1,5 @@
+"C:\Program Files\Unity\Hub\Editor\2021.1.3f1\Editor\Unity.exe" -quit -batchmode -nographics -silent-crashes -logFile -projectPath . -executeMethod UnitTestBuilder.BuildUnitTest /headless /ScriptBackend Mono2x /BuildTarget StandaloneWindows64
+
+"bin/UnitTest/StandaloneWindows64_Mono2x/test.exe"
+
+pause
